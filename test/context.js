ad0dd3cc39c27d71149dcaa8f7f7807cd7e3ed8e
TJ Holowaychuk <tj@vision-media.ca>
2013-08-27 20:54:13 -0700

    add Context#error(). Closes #31

--- test/context.js
+++ test/context.js
@@ -21,6 +21,47 @@ function context(req, res) {
   return ctx;
 }
 
+describe('ctx.error(msg)', function(){
+  it('should set .status to 500', function(done){
+    var ctx = context();
+
+    try {
+      ctx.error('boom');      
+    } catch (err) {
+      assert(500 == err.status);
+      done();
+    }
+  })
+})
+
+describe('ctx.error(msg, status)', function(){
+  it('should throw an error', function(done){
+    var ctx = context();
+
+    try {
+      ctx.error('name required', 400);
+    } catch (err) {
+      assert('name required' == err.message);
+      assert(400 == err.status);
+      done();
+    }
+  })
+})
+
+describe('ctx.error(status)', function(){
+  it('should throw an error', function(done){
+    var ctx = context();
+
+    try {
+      ctx.error(400);
+    } catch (err) {
+      assert('Bad Request' == err.message);
+      assert(400 == err.status);
+      done();
+    }
+  })
+})
+
 describe('ctx.length', function(){
   describe('when Content-Length is defined', function(){
     it('should return a number', function(){
