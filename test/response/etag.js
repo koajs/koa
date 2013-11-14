2f74207e6c86b06037dd159899e3b0c3ac7b4954
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-13 19:59:49 -0800

    response.etag and response.lastModified

---
+++ test/response/etag.js
@@ -0,0 +1,28 @@
+
+var response = require('../response');
+
+describe('res.etag', function(){
+  it('should not modify an etag with quotes', function(){
+    var res = response();
+    res.etag = '"asdf"';
+    res.header.etag.should.equal('"asdf"');
+  })
+
+  it('should not modify a weak etag', function(){
+    var res = response();
+    res.etag = 'W/"asdf"';
+    res.header.etag.should.equal('W/"asdf"');
+  })
+
+  it('should add quotes around an etag if necessary', function(){
+    var res = response();
+    res.etag = 'asdf';
+    res.header.etag.should.equal('"asdf"');
+  })
+
+  it('should get the etag', function(){
+    var res = response();
+    res.set('etag', '"asdf"');
+    res.etag.should.equal('"asdf"');
+  })
+})
