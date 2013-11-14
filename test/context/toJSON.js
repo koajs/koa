35a0c1d2bbf08ba75fc26b60ba79e555d9180fdd
TJ Holowaychuk <tj@vision-media.ca>
2013-11-14 11:30:56 -0800

    add {request,response,context}#toJSON()

---
+++ test/context/toJSON.js
@@ -0,0 +1,35 @@
+
+var context = require('../context');
+
+describe('ctx.toJSON()', function(){
+  it('should return a json representation', function(){
+    var ctx = context();
+
+    ctx.req.method = 'POST';
+    ctx.req.url = '/items';
+    ctx.req.headers['content-type'] = 'text/plain';
+    ctx.status = 200;
+    ctx.body = '<p>Hey</p>';
+
+    var obj = ctx.toJSON();
+    var req = obj.request;
+    var res = obj.response;
+
+    req.should.eql({
+      method: 'POST',
+      url: '/items',
+      header: {
+        'content-type': 'text/plain'
+      }
+    });
+
+    res.should.eql({
+      status: 200,
+      string: 'OK',
+      header: {
+        'content-type': 'text/html; charset=utf-8',
+        'content-length': '10'
+      }
+    });
+  })
+})
