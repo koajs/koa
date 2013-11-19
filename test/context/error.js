7d9c6ba66c38c580b3d7a628a364661a5afcd2d5
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-18 17:38:12 -0800

    context: .error() -> .throw()
    .error() still works for compatibility, but it will be removed in the
    future. closes #94

--- test/context/error.js
+++ test/context/error.js
@@ -2,12 +2,12 @@
 var context = require('../context');
 var assert = require('assert');
 
-describe('ctx.error(msg)', function(){
+describe('ctx.throw(msg)', function(){
   it('should set .status to 500', function(done){
     var ctx = context();
 
     try {
-      ctx.error('boom');
+      ctx.throw('boom');
     } catch (err) {
       assert(500 == err.status);
       done();
@@ -15,12 +15,12 @@ describe('ctx.error(msg)', function(){
   })
 })
 
-describe('ctx.error(msg, status)', function(){
+describe('ctx.throw(msg, status)', function(){
   it('should throw an error', function(done){
     var ctx = context();
 
     try {
-      ctx.error('name required', 400);
+      ctx.throw('name required', 400);
     } catch (err) {
       assert('name required' == err.message);
       assert(400 == err.status);
@@ -29,12 +29,12 @@ describe('ctx.error(msg, status)', function(){
   })
 })
 
-describe('ctx.error(status)', function(){
+describe('ctx.throw(status)', function(){
   it('should throw an error', function(done){
     var ctx = context();
 
     try {
-      ctx.error(400);
+      ctx.throw(400);
     } catch (err) {
       assert('Bad Request' == err.message);
       assert(400 == err.status);
