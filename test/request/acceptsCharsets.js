c24ab00b231ed2271bb0b30df86b29e48a4d3b77
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-26 14:49:07 -0800

    req.acceptsCharsets - default to first type if header not set

--- test/request/acceptsCharsets.js
+++ test/request/acceptsCharsets.js
@@ -20,10 +20,29 @@ describe('ctx.acceptsCharsets()', function(){
   })
 
   describe('with multiple arguments', function(){
-    it('should return the best fit', function(){
-      var ctx = context();
-      ctx.req.headers['accept-charset'] = 'utf-8, iso-8859-1;q=0.2, utf-7;q=0.5';
-      ctx.acceptsCharsets('utf-7', 'utf-8').should.equal('utf-8');
+    describe('when Accept-Charset is populated', function(){
+      describe('if any types match', function(){
+        it('should return the best fit', function(){
+          var ctx = context();
+          ctx.req.headers['accept-charset'] = 'utf-8, iso-8859-1;q=0.2, utf-7;q=0.5';
+          ctx.acceptsCharsets('utf-7', 'utf-8').should.equal('utf-8');
+        })
+      })
+
+      describe('if no types match', function(){
+        it('should return false', function(){
+          var ctx = context();
+          ctx.req.headers['accept-charset'] = 'utf-8, iso-8859-1;q=0.2, utf-7;q=0.5';
+          ctx.acceptsCharsets('utf-16').should.be.false;
+        })
+      })
+    })
+
+    describe('when Accept-Charset is not populated', function(){
+      it('should return the first type', function(){
+        var ctx = context();
+        ctx.acceptsCharsets('utf-7', 'utf-8').should.equal('utf-7');
+      })
     })
   })
 
