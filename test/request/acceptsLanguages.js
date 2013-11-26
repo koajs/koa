e447e731b684d74c1c56e7e4c80c1dad5b263d4b
Jonathan Ong <jonathanrichardong@gmail.com>
2013-11-26 14:52:24 -0800

    req.acceptsLanguage - default to first type fi header not set

--- test/request/acceptsLanguages.js
+++ test/request/acceptsLanguages.js
@@ -20,10 +20,29 @@ describe('ctx.acceptsLanguages(langs)', function(){
   })
 
   describe('with multiple arguments', function(){
-    it('should return the best fit', function(){
-      var ctx = context();
-      ctx.req.headers['accept-language'] = 'en;q=0.8, es, pt';
-      ctx.acceptsLanguages('es', 'en').should.equal('es');
+    describe('when Accept-Language is populated', function(){
+      describe('if any types types match', function(){
+        it('should return the best fit', function(){
+          var ctx = context();
+          ctx.req.headers['accept-language'] = 'en;q=0.8, es, pt';
+          ctx.acceptsLanguages('es', 'en').should.equal('es');
+        })
+      })
+
+      describe('if no types match', function(){
+        it('should return false', function(){
+          var ctx = context();
+          ctx.req.headers['accept-language'] = 'en;q=0.8, es, pt';
+          ctx.acceptsLanguages('fr', 'au').should.be.false;
+        })
+      })
+    })
+
+    describe('when Accept-Language is not populated', function(){
+      it('should return the first type', function(){
+        var ctx = context();
+        ctx.acceptsLanguages('es', 'en').should.equal('es');
+      })
     })
   })
 
