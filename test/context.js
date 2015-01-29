68843e0cb3758fbba4e0e6a75640e08a40972559
Jesus Rodriguez <foxandxss@gmail.com>
2015-01-29 23:06:23 +0100

    Remove unused imports and exports

--- test/context.js
+++ test/context.js
@@ -1,8 +1,5 @@
 
 var Stream = require('stream');
-var context = require('../lib/context');
-var request = require('../lib/request');
-var response = require('../lib/response');
 var koa = require('..');
 
 exports = module.exports = function(req, res){
@@ -13,10 +10,6 @@ exports = module.exports = function(req, res){
   res.setHeader = function(k, v){ res._headers[k.toLowerCase()] = v };
   res.removeHeader = function(k, v){ delete res._headers[k.toLowerCase()] };
   return koa().createContext(req, res);
-}
-
-exports.context = function(req, res){
-  return exports(req, res);
 }
 
 exports.request = function(req, res){
