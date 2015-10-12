15656b3d63243fd166c2c804739f54549d2c59b9
Tejas Manohar <me@tejas.io>
2015-10-12 00:10:49 -0500

    add tests for app.context()

--- test/application.js
+++ test/application.js
@@ -10,34 +10,6 @@ const koa = require('..');
 const fs = require('fs');
 const AssertionError = assert.AssertionError;
 
-describe('app.context', function(){
-  const app1 = koa();
-  app1.context.msg = 'hello';
-  const app2 = koa();
-
-  it('should merge properties', function(done){
-    app1.use(function *(next){
-      assert.equal(this.msg, 'hello')
-      this.status = 204
-    });
-
-    request(app1.listen())
-    .get('/')
-    .expect(204, done);
-  })
-
-  it('should not affect the original prototype', function(done){
-    app2.use(function *(next){
-      assert.equal(this.msg, undefined)
-      this.status = 204;
-    });
-
-    request(app2.listen())
-    .get('/')
-    .expect(204, done);
-  })
-})
-
 describe('app.request', function(){
   const app1 = koa();
   app1.request.message = 'hello';
---
+++ test/application/context.js
@@ -0,0 +1,34 @@
+
+'use strict';
+
+const request = require('supertest');
+const assert = require('assert');
+const koa = require('../..');
+
+describe('app.context', function(){
+  const app1 = koa();
+  app1.context.msg = 'hello';
+  const app2 = koa();
+
+  it('should merge properties', function(done){
+    app1.use(function *(next){
+      assert.equal(this.msg, 'hello')
+      this.status = 204
+    });
+
+    request(app1.listen())
+    .get('/')
+    .expect(204, done);
+  })
+
+  it('should not affect the original prototype', function(done){
+    app2.use(function *(next){
+      assert.equal(this.msg, undefined)
+      this.status = 204;
+    });
+
+    request(app2.listen())
+    .get('/')
+    .expect(204, done);
+  })
+})
