4d2f88cbe263413c89a0c3097465cf4a141598d4
Tejas Manohar <me@tejas.io>
2015-10-11 20:28:39 -0500

    remove unused requires/imports

--- test/application/respond.js
+++ test/application/respond.js
@@ -5,10 +5,8 @@ const stderr = require('test-console').stderr;
 const request = require('supertest');
 const statuses = require('statuses');
 const assert = require('assert');
-const http = require('http');
 const koa = require('../..');
 const fs = require('fs');
-const AssertionError = assert.AssertionError;
 
 describe('app.respond', function(){
   describe('when this.respond === false', function(){
