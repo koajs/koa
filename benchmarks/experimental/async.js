a4493aa819f5bf969ff766324548d18160c7d272
Tejas Manohar <me@tejas.io>
2015-10-12 02:58:55 -0500

    refactor benchmarks

--- benchmarks/experimental/async.js
+++ benchmarks/experimental/async.js
@@ -1,28 +1,27 @@
 
-'use strict';
+'use strict'
 
-const http = require('http');
-const koa = require('../..');
-const app = koa();
+const koa = require('../..')
+const app = koa()
 
-app.experimental = true;
+app.experimental = true
 
 // number of middleware
 
-let n = parseInt(process.env.MW || '1', 10);
-console.log(`  ${n} async middleware`);
+let n = parseInt(process.env.MW || '1', 10)
+console.log(`  ${n} async middleware`)
 
 while (n--) {
-  app.use(async function (next){
-    await next;
-  });
+  app.use(async function (next) {
+    await next
+  })
 }
 
-const body = new Buffer('Hello World');
+const body = new Buffer('Hello World')
 
-app.use(async function (next){
-  await next;
-  this.body = body;
-});
+app.use(async function (next) {
+  await next
+  this.body = body
+})
 
-app.listen(3333);
+app.listen(3333)
--- benchmarks/middleware.js
+++ benchmarks/middleware.js
@@ -1,26 +1,24 @@
+'use strict'
 
-'use strict';
-
-const http = require('http');
-const koa = require('..');
-const app = koa();
+const koa = require('..')
+const app = koa()
 
 // number of middleware
 
-let n = parseInt(process.env.MW || '1', 10);
-console.log(`  ${n} middleware`);
+let n = parseInt(process.env.MW || '1', 10)
+console.log(`  ${n} middleware`)
 
 while (n--) {
-  app.use(function *(next){
-    yield *next;
-  });
+  app.use(function *(next) {
+    yield* next
+  })
 }
 
-const body = new Buffer('Hello World');
+const body = new Buffer('Hello World')
 
-app.use(function *(next){
-  yield *next;
-  this.body = body;
-});
+app.use(function *(next) {
+  yield* next
+  this.body = body
+})
 
-app.listen(3333);
+app.listen(3333)
