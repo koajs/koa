c14b321c93eb6ddce7285f24af66240b9af4ac4b
Michaël Zasso <mic.besace@gmail.com>
2015-10-28 16:46:00 +0100

    test: remove need of NODE_ENV environment variable
    This make the tests runnable on Windows

--- test/application/toJSON.js
+++ test/application/toJSON.js
@@ -5,8 +5,13 @@ const Koa = require('../..');
 
 describe('app.toJSON()', function(){
   it('should work', function(){
+    const env = process.env.NODE_ENV;
+    process.env.NODE_ENV = 'test';
+
     const app = new Koa();
     const obj = app.toJSON();
+
+    process.env.NODE_ENV = env;
 
     obj.should.eql({
       subdomainOffset: 2,
