aa1fbbff4a4b4989dd5b8c8f1cc30f833341b7c8
TJ Holowaychuk <tj@vision-media.ca>
2015-10-29 09:51:11 -0700

    fix Application.inspect() – missing .proxy value. Closes #563
    fix trailing comma

--- test/application/inspect.js
+++ test/application/inspect.js
@@ -1,12 +1,14 @@
 
 'use strict';
 
+const assert = require('assert');
 const Koa = require('../..');
 
 describe('app.inspect()', function(){
   it('should work', function(){
     const app = new Koa();
     const util = require('util');
-    util.inspect(app);
+    const str = util.inspect(app);
+    assert.equal("{ subdomainOffset: 2, proxy: false, env: 'test' }", str);
   });
 });
