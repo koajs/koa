08057e386a48f9bde50e5568f7b97ad691b162d8
dead_horse <dead_horse@qq.com>
2015-11-04 13:22:16 +0800

    ensure parseurl always working as expected

--- test/request/path.js
+++ test/request/path.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const context = require('../helpers/context');
+const parseurl = require('parseurl');
 
 describe('ctx.path', () => {
   it('should return the pathname', () => {
@@ -27,5 +28,12 @@ describe('ctx.path=', () => {
     ctx.url.should.equal('/logout');
     ctx.originalUrl.should.equal('/login');
     ctx.request.originalUrl.should.equal('/login');
+  });
+
+  it('should not affect parseurl', () => {
+    const ctx = context({ url: '/login?foo=bar' });
+    ctx.path = '/login';
+    const url = parseurl(ctx.req);
+    url.path.should.equal('/login?foo=bar');
   });
 });
