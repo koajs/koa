51764bbdbdb20e4f31057137a8f0b00d12a84539
Ian Macalinao <me@ian.pw>
2015-11-18 21:01:27 -0600

    Export app-specific context, request, and responses

--- test/application/context.js
+++ test/application/context.js
@@ -7,7 +7,7 @@ const Koa = require('../..');
 
 describe('app.context', () => {
   const app1 = new Koa();
-  app1.context.msg = 'hello';
+  app1.Context.prototype.msg = 'hello';
   const app2 = new Koa();
 
   it('should merge properties', done => {
