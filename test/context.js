f818bf4b535596fc42b6c6d753e6a172eb0094dc
Jonathan Ong <jonathanrichardong@gmail.com>
2016-02-28 17:12:58 -0800

    proxy-addr + forwarded

--- test/context.js
+++ test/context.js
@@ -6,8 +6,9 @@ var koa = require('..');
 
 exports = module.exports = function(req, res){
   var socket = new Stream.Duplex();
-  req = req || { headers: {}, socket: socket, __proto__: Stream.Readable.prototype };
-  res = res || { _headers: {}, socket: socket, __proto__: Stream.Writable.prototype };
+  socket.remoteAddress = '127.0.0.1';
+  req = req || { headers: {}, socket: socket, connection: socket, __proto__: Stream.Readable.prototype };
+  res = res || { _headers: {}, socket: socket, connection: socket, __proto__: Stream.Writable.prototype };
   res.getHeader = function(k){ return res._headers[k.toLowerCase()] };
   res.setHeader = function(k, v){ res._headers[k.toLowerCase()] = v };
   res.removeHeader = function(k, v){ delete res._headers[k.toLowerCase()] };
