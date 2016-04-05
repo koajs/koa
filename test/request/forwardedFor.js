b7a25ef04a44838aa392e8f8a2a932b89841d2ca
Lee Bousfield <ljbousfield@gmail.com>
2016-04-05 12:58:30 -0600

    Rename request.ips to request.forwardedFor

--- test/request/forwardedFor.js
+++ test/request/forwardedFor.js
@@ -1,16 +1,17 @@
 
 'use strict';
 
+const assert = require('assert');
 const request = require('../helpers/context').request;
 
-describe('req.ips', () => {
+describe('req.forwardedFor', () => {
   describe('when X-Forwarded-For is present', () => {
     describe('and proxy is not trusted', () => {
       it('should be ignored', () => {
         const req = request();
         req.app.proxy = false;
         req.header['x-forwarded-for'] = '127.0.0.1,127.0.0.2';
-        req.ips.should.eql([]);
+        req.forwardedFor.should.eql([]);
       });
     });
 
@@ -19,8 +20,18 @@ describe('req.ips', () => {
         const req = request();
         req.app.proxy = true;
         req.header['x-forwarded-for'] = '127.0.0.1,127.0.0.2';
-        req.ips.should.eql(['127.0.0.1', '127.0.0.2']);
+        req.forwardedFor.should.eql(['127.0.0.1', '127.0.0.2']);
       });
     });
+  });
+
+  it('should also be usable with deprecated .ips', () => {
+    let deprecated = false;
+    process.on('deprecation', message => deprecated = true);
+    const req = request();
+    req.app.proxy = true;
+    req.header['x-forwarded-for'] = '127.0.0.1,127.0.0.2';
+    req.ips.should.eql(['127.0.0.1', '127.0.0.2']);
+    assert(deprecated);
   });
 });
