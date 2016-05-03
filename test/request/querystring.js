d3874b7471aa63dc3351d747d3c1fba81f3eb175
Qi Yu <njuyuqi@gmail.com>
2016-05-03 14:39:07 +0800

    remove the local should and change to a better way

--- test/request/querystring.js
+++ test/request/querystring.js
@@ -3,7 +3,6 @@
 
 var context = require('../context');
 var parseurl = require('parseurl');
-var should = require('should');
 
 describe('ctx.querystring', function(){
   it('should return the querystring', function(){
@@ -33,8 +32,9 @@ describe('ctx.querystring=', function(){
     ctx.querystring = 'page=2&color=blue';
     ctx.url.should.equal('/store/shoes?page=2&color=blue');
     ctx.search.should.equal('?page=2&color=blue');
-    should(ctx.query).have.property('page', '2');
-    should(ctx.query).have.property('color', 'blue');
+    Object.keys(ctx.query).should.eql(['page', 'color']);
+    ctx.query.page.should.equal('2');
+    ctx.query.color.should.equal('blue');
   })
 
   it('should change .url but not .originalUrl', function(){
