7ea148f6a4b45505729ec02b3dfae3fe4e8bc4cf
dead_horse <dead_horse@qq.com>
2016-07-21 17:58:11 +0800

    fix(response): correct response.writable logic

--- test/response/writeable.js
+++ test/response/writeable.js
@@ -1,19 +1,105 @@
 
 'use strict';
 
-var response = require('../context').response;
+var should = require('should');
+var koa = require('../../');
+var net = require('net');
 
 describe('res.writable', function(){
-  it('should return the request is writable', function(){
-    var res = response();
-    res.writable.should.be.ok;
+  describe('when continuous requests in one persistent connection', function() {
+    function requestTwice(server, done) {
+      let port = server.address().port;
+      var buf = new Buffer('GET / HTTP/1.1\r\nHost: localhost:' + port + '\r\nConnection: keep-alive\r\n\r\n');
+      var client = net.connect(port);
+      var datas = [];
+      client
+        .on('error', done)
+        .on('data', function(data) {
+          datas.push(data);
+        })
+        .on('end', function() {
+          done(null, datas);
+        });
+      setImmediate(function() {
+        client.write(buf);
+      });
+      setImmediate(function() {
+        client.write(buf);
+      });
+      setTimeout(function() {
+        client.end();
+      }, 100);
+    }
+
+    it('should always writable and response all requests', function(done) {
+      var app = koa();
+      var count = 0;
+      app.use(function*() {
+        count++;
+        this.body = 'request ' + count + ', writable: ' + this.writable;
+      });
+
+      var server = app.listen();
+      requestTwice(server, function(err, datas) {
+        var responses = Buffer.concat(datas).toString();
+        responses.should.match(/request 1, writable: true/);
+        responses.should.match(/request 2, writable: true/);
+        done();
+      });
+    })
   })
 
-  describe('when res.socket not present', function (){
-    it('should return the request is not writable', function (){
-      var res = response();
-      res.res.socket = null;
-      res.writable.should.not.be.ok;
+  describe('when socket closed before response sent', function() {
+    function requsetClosed(server) {
+      let port = server.address().port;
+      var buf = new Buffer('GET / HTTP/1.1\r\nHost: localhost:' + port + '\r\nConnection: keep-alive\r\n\r\n');
+      var client = net.connect(port);
+      setImmediate(function() {
+        client.write(buf);
+        client.end();
+      });
+    }
+
+    it('should not writable', function(done) {
+      var app = koa();
+      app.use(function*() {
+        yield sleep(1000);
+        if (this.writable) return done(new Error('this.writable should not be true'));
+        done();
+      });
+      var server = app.listen();
+      requsetClosed(server);
+    })
+  })
+
+  describe('when resposne finished', function() {
+    function request(server) {
+      let port = server.address().port;
+      var buf = new Buffer('GET / HTTP/1.1\r\nHost: localhost:' + port + '\r\nConnection: keep-alive\r\n\r\n');
+      var client = net.connect(port);
+      setImmediate(function() {
+        client.write(buf);
+      });
+      setTimeout(function() {
+        client.end();
+      }, 100);
+    }
+
+    it('should not writable', function(done) {
+      var app = koa();
+      app.use(function*() {
+        this.res.end();
+        if (this.writable) return done(new Error('this.writable should not be true'));
+        done();
+      });
+      var server = app.listen();
+      request(server);
     })
   })
 })
+
+function sleep(time) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, time);
+  });
+}
