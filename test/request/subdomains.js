27caff3d59090715549865b1cc713861069df233
Adam Lau <skyros@gmail.com>
2016-09-07 16:20:44 +0800

    fix: should get subdomains from hostname instead (#809)
    This helps avoid dealing with the port in this.host

--- test/request/subdomains.js
+++ test/request/subdomains.js
@@ -19,9 +19,9 @@ describe('req.subdomains', function(){
     req.subdomains.should.eql([]);
   })
 
-  it('should check if the host is an ip address', function(){
+  it('should check if the host is an ip address, even with a port', function(){
     var req = request();
-    req.header.host = '127.0.0.1';
+    req.header.host = '127.0.0.1:3000';
     req.subdomains.should.eql([]);
   });
 })
