91c403a541ba91e3ba10afdfa8b28adeae74fbbe
Ilkka Oksanen <iao@iki.fi>
2017-02-21 07:52:57 +0100

    response: don't access res._headers directly when helpers exist
    res._headers is considered internal to node itself. Its value will
    change in a backwards incompatible way in the future node releases.
    
    Use the documented helper functions instead when they are available.

--- test/response/header.js
+++ test/response/header.js
@@ -10,6 +10,16 @@ describe('res.header', function(){
     res.header.should.eql({ 'x-foo': 'bar' });
   })
 
+  it('should use res.getHeaders helper when it is available', function(){
+    var res = response(null, {
+      _headers: {},
+      getHeaders: function(){
+        return { 'x-foo': 'baz' }
+      }
+    });
+    res.header.should.eql({ 'x-foo': 'baz' });
+  })
+
   describe('when res._headers not present', function (){
     it('should return empty object', function (){
       var res = response();
