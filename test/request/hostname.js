477c5aa75d3dd0962f236528b4293f65976d3a73
Martin fl0w Iwanowski <martin@iwanowski.se>
2017-06-17 23:25:35 +0200

    Use node 7+ WHATWG parser for hostname, fixes #1002

--- test/request/hostname.js
+++ test/request/hostname.js
@@ -18,6 +18,32 @@ describe('req.hostname', () => {
     });
   });
 
+  describe('with IPv6 in host', () => {
+    it('should parse localhost void of port', () => {
+      const req = request();
+      req.header.host = '[::1]';
+      assert.equal(req.hostname, '[::1]');
+    });
+
+    it('should parse localhost with port 80', () => {
+      const req = request();
+      req.header.host = '[::1]:80';
+      assert.equal(req.hostname, '[::1]');
+    });
+
+    it('should parse localhost with non special schema port', () => {
+      const req = request();
+      req.header.host = '[::1]:1337';
+      assert.equal(req.hostname, '[::1]');
+    });
+
+    it('should reduce IPv6 with non special schema port, as hostname', () => {
+      const req = request();
+      req.header.host = '[2001:cdba:0000:0000:0000:0000:3257:9652]:1337';
+      assert.equal(req.hostname, '[2001:cdba::3257:9652]');
+    });
+  });
+
   describe('when X-Forwarded-Host is present', () => {
     describe('and proxy is not trusted', () => {
       it('should be ignored', () => {
