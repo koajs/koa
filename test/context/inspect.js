7073f2c00b7e0210741152ca5a4cfac185767b63
Luke Bousfield <lukebousfield@Lukes-MacBook-Pro.local>
2017-06-25 21:25:16 -0600

    Add tests

--- test/context/inspect.js
+++ test/context/inspect.js
@@ -1,6 +1,7 @@
 
 'use strict';
 
+const prototype = require('../../lib/context');
 const assert = require('assert');
 const context = require('../helpers/context');
 
@@ -10,5 +11,10 @@ describe('ctx.inspect()', () => {
     const toJSON = ctx.toJSON(ctx);
 
     assert.deepEqual(toJSON, ctx.inspect());
+  });
+
+  // console.log(require.cache) will call prototype.inspect()
+  it('should not crash when called on the prototype', () => {
+    assert.deepEqual(prototype, prototype.inspect());
   });
 });
