31a540ed773fb07e605ad44a203e2dc7a7536eca
Qi Yu <njuyuqi@gmail.com>
2017-07-31 15:01:47 +0800

    add bench for async/await

--- benchmarks/middleware.js
+++ benchmarks/middleware.js
@@ -7,14 +7,24 @@ const app = new Koa();
 // number of middleware
 
 let n = parseInt(process.env.MW || '1', 10);
-console.log(`  ${n} middleware`);
+let useAsync = process.env.USE_ASYNC === 'true';
+
+console.log(`  ${n}${useAsync ? ' async' : ''} middleware`);
 
 while (n--) {
-  app.use((ctx, next) => next());
+  if (useAsync) {
+    app.use(async (ctx, next) => await next());
+  } else {
+    app.use((ctx, next) => next());
+  }
 }
 
 const body = Buffer.from('Hello World');
 
-app.use((ctx, next) => next().then(() => ctx.body = body));
+if (useAsync) {
+  app.use(async (ctx, next) => { await next(); ctx.body = body; });
+} else {
+  app.use((ctx, next) => next().then(() => ctx.body = body));
+}
 
 app.listen(3333);
