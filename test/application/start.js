a34595ca63317b5e2dbc42f3908af4d67f2aac15
Ilia Potuzhnov <ipotuzhnov@gmail.com>
2017-11-27 22:59:15 -0500

    Application: app.start rejects when server emits an error

--- test/application/start.js
+++ test/application/start.js
@@ -3,21 +3,50 @@
 const assert = require('assert');
 const Koa = require('../..');
 const http = require('http');
+const EventEmitter = require('events');
 
 describe('app.start', () => {
-  it('should eventually return a listening http server', async () => {
-    const app = new Koa();
+  describe('when server starts successfully', () => {
+    it('should eventually return a listening http server', async () => {
+      const app = new Koa();
 
-    const server = await app.start();
+      const server = await app.start();
 
-    assert.equal(server instanceof http.Server, true);
-    assert.equal(server.listening, true);
+      assert.equal(server instanceof http.Server, true);
+      assert.equal(server.listening, true);
 
-    await new Promise((resolve, reject) => {
-      server.close(err => {
-        if (err) return reject(err);
-        resolve();
+      await new Promise((resolve, reject) => {
+        server.close(err => {
+          if (err) return reject(err);
+          resolve();
+        });
       });
+    });
+  });
+
+  describe('when server emits an error', () => {
+    const error = new Error('EADDRINUSE');
+
+    const originalCreateServer = http.createServer;
+    beforeEach(() => {
+      const server = new EventEmitter();
+      http.createServer = () => server;
+      server.listen = () => {
+        process.nextTick(() => server.emit('error', error));
+      };
+    });
+    afterEach(() => {
+      http.createServer = originalCreateServer;
+    });
+
+    it('should eventually throw an error', async () => {
+      const app = new Koa();
+
+      try {
+        await app.start();
+      } catch (e) {
+        assert.strictEqual(e, error);
+      }
     });
   });
 });
