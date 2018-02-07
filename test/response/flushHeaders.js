c6109ac81574f3fa0c90733fb979f634a8d95fd4
dead-horse <dead_horse@qq.com>
2018-02-07 15:49:47 +0800

    feat: ignore set header/status when header sent

--- test/response/flushHeaders.js
+++ test/response/flushHeaders.js
@@ -61,39 +61,27 @@ describe('ctx.flushHeaders()', () => {
       .expect('Body');
   });
 
-  it('should fail to set the headers after flushHeaders', async () => {
+  it('should ignore set header after flushHeaders', async () => {
     const app = new Koa();
 
     app.use((ctx, next) => {
       ctx.status = 401;
       ctx.res.setHeader('Content-Type', 'text/plain');
       ctx.flushHeaders();
-      ctx.body = '';
-      try {
-        ctx.set('X-Shouldnt-Work', 'Value');
-      } catch (err) {
-        ctx.body += 'ctx.set fail ';
-      }
-      try {
-        ctx.status = 200;
-      } catch (err) {
-        ctx.body += 'ctx.status fail ';
-      }
-      try {
-        ctx.length = 10;
-      } catch (err) {
-        ctx.body += 'ctx.length fail';
-      }
+      ctx.body = 'foo';
+      ctx.set('X-Shouldnt-Work', 'Value');
+      ctx.remove('Content-Type');
+      ctx.vary('Content-Type');
     });
 
     const server = app.listen();
     const res = await request(server)
       .get('/')
       .expect(401)
-      .expect('Content-Type', 'text/plain')
-      .expect('ctx.set fail ctx.status fail ctx.length fail');
+      .expect('Content-Type', 'text/plain');
 
     assert.equal(res.headers['x-shouldnt-work'], undefined, 'header set after flushHeaders');
+    assert.equal(res.headers.vary, undefined, 'header set after flushHeaders');
   });
 
   it('should flush headers first and delay to send data', done => {
@@ -133,5 +121,31 @@ describe('ctx.flushHeaders()', () => {
       .on('error', done)
       .end();
     });
+  });
+
+  it('should catch stream error', done => {
+    const PassThrough = require('stream').PassThrough;
+    const app = new Koa();
+    app.once('error', err => {
+      assert(err.message === 'mock error');
+      done();
+    });
+
+    app.use(ctx => {
+      ctx.type = 'json';
+      ctx.status = 200;
+      ctx.headers['Link'] = '</css/mycss.css>; as=style; rel=preload, <https://img.craftflair.com>; rel=preconnect; crossorigin';
+      ctx.length = 20;
+      ctx.flushHeaders();
+      const stream = ctx.body = new PassThrough();
+
+      setTimeout(() => {
+        stream.emit('error', new Error('mock error'));
+      }, 100);
+    });
+
+    const server = app.listen();
+
+    request(server).get('/').end();
   });
 });
