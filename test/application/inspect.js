0099eef01cd55551bac9fa2c48c85add5c6884af
Ruben Bridgewater <ruben@bridgewater.de>
2018-05-23 09:41:22 +0200

    fix: use non deprecated custom inspect
    Custom inspection with the `inspect` property is deprecated and will
    not work in Node.js 11 anymore. This fixes it by using the custom
    inspect symbol where existent and falls back to the old style in case
    it does not exist.

--- test/application/inspect.js
+++ test/application/inspect.js
@@ -2,13 +2,20 @@
 'use strict';
 
 const assert = require('assert');
+const util = require('util');
 const Koa = require('../..');
+const app = new Koa();
 
 describe('app.inspect()', () => {
   it('should work', () => {
-    const app = new Koa();
-    const util = require('util');
     const str = util.inspect(app);
     assert.equal("{ subdomainOffset: 2, proxy: false, env: 'test' }", str);
+  });
+
+  it('should return a json representation', () => {
+    assert.deepEqual(
+      { subdomainOffset: 2, proxy: false, env: 'test' },
+      app.inspect()
+    );
   });
 });
