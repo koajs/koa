53daa01d0c483d8db0d0014cfd844b5a8c832fd7
Ruben Bridgewater <ruben@bridgewater.de>
2018-05-27 14:36:47 +0200

    test: improve coverage

--- test/context/inspect.js
+++ test/context/inspect.js
@@ -3,6 +3,7 @@
 
 const prototype = require('../../lib/context');
 const assert = require('assert');
+const util = require('util');
 const context = require('../helpers/context');
 
 describe('ctx.inspect()', () => {
@@ -11,10 +12,12 @@ describe('ctx.inspect()', () => {
     const toJSON = ctx.toJSON(ctx);
 
     assert.deepEqual(toJSON, ctx.inspect());
+    assert.deepEqual(util.inspect(toJSON), util.inspect(ctx));
   });
 
   // console.log(require.cache) will call prototype.inspect()
   it('should not crash when called on the prototype', () => {
     assert.deepEqual(prototype, prototype.inspect());
+    assert.deepEqual(util.inspect(prototype.inspect()), util.inspect(prototype));
   });
 });
