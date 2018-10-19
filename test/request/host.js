350f5e1fcf29206a178d6e0af1f100a5a19e5629
Martin Michaelis <code@mgjm.de>
2018-10-19 16:08:17 +0200

    fix: use :authority header of http2 requests as host (fixes #1174)

--- test/request/host.js
+++ test/request/host.js
@@ -18,6 +18,39 @@ describe('req.host', () => {
     });
   });
 
+  describe('when less then HTTP/2', () => {
+    it('should not use :authority header', () => {
+      const req = request({
+        'httpVersionMajor': 1,
+        'httpVersion': '1.1'
+      });
+      req.header[':authority'] = 'foo.com:3000';
+      req.header.host = 'bar.com:8000';
+      assert.equal(req.host, 'bar.com:8000');
+    });
+  });
+
+  describe('when HTTP/2', () => {
+    it('should use :authority header', () => {
+      const req = request({
+        'httpVersionMajor': 2,
+        'httpVersion': '2.0'
+      });
+      req.header[':authority'] = 'foo.com:3000';
+      req.header.host = 'bar.com:8000';
+      assert.equal(req.host, 'foo.com:3000');
+    });
+
+    it('should use host header as fallback', () => {
+      const req = request({
+        'httpVersionMajor': 2,
+        'httpVersion': '2.0'
+      });
+      req.header.host = 'bar.com:8000';
+      assert.equal(req.host, 'bar.com:8000');
+    });
+  });
+
   describe('when X-Forwarded-Host is present', () => {
     describe('and proxy is not trusted', () => {
       it('should be ignored', () => {
