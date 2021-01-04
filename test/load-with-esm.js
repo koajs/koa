b5472f4cbb87349becae36b4a9ad5f76a825abb8
miwnwski <m@iwnw.ski>
2021-01-04 16:10:24 +0100

    fix: make ESM transpiled CommonJS play nice for TS folks, fix #1513 (#1518)

--- test/load-with-esm.js
+++ test/load-with-esm.js
@@ -28,9 +28,22 @@ describe('Load with esm', () => {
     for (const k of ['prototype', 'length', 'name']) {
       required.delete(k);
     }
-    exported.delete('default');
+
+    // Commented out to "fix" CommonJS, ESM, bundling issue.
+    // @see https://github.com/koajs/koa/issues/1513
+    // exported.delete('default');
 
     assert.strictEqual(exported.size, required.size);
     assert.strictEqual([...exported].every(property => required.has(property)), true);
+  });
+
+  it('CommonJS exports default property', async() => {
+    const required = require('../');
+    assert.strictEqual(required.hasOwnProperty('default'), true);
+  });
+
+  it('CommonJS exports default property referencing self', async() => {
+    const required = require('../');
+    assert.strictEqual(required.default, required);
   });
 });
