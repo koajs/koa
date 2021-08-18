4545b59aa521e377a4ebb0ccbb8abec01981fbf4
jongleberry <jonathanrichardong@gmail.com>
2021-08-17 21:27:08 -0700

    test: switch tests to jest

---
+++ __tests__/request/headers.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+const assert = require('assert');
+const request = require('../../test-helpers/context').request;
+
+describe('req.headers', () => {
+  it('should return the request header object', () => {
+    const req = request();
+    assert.deepEqual(req.headers, req.req.headers);
+  });
+
+  it('should set the request header object', () => {
+    const req = request();
+    req.headers = {'X-Custom-Headerfield': 'Its one header, with headerfields'};
+    assert.deepEqual(req.headers, req.req.headers);
+  });
+});
